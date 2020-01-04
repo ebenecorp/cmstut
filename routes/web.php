04ce2ca469cd5ef8601019d2a19ffca5fe7a6e8b
unknown <ebene2009@gmail.com>
2020-01-04 17:12:01 +0100

    first commmit

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/about', function () {
+//     return "HI, I am About Page";
+// });
+
+
+// Route::get('/contact', function () {
+//     return  "HI, YOU ARE VIEWING CONTACT US";
+// });
+
+// Route::get('/post/{id}', function($id){
+
+//     return " This is post number ".$id;
+// });
+
+// Route::get('admin/posts/example', array('as'=>'admin.home ',function(){
+//         $url = route('admin.home');
+
+//         return "this url is ". $url;
+
+// }));
+
+// Route::get('/post', 'PostController@index');
+
+// Route::get('/post/{id}', 'PostController@index');
+
+// Route::resource('post', 'PostController');
+
+ Route::get('/contact', 'PostController@contact');
+//Route::get('/contact/{id}', 'PostController@contact');
+
+Route::get('/post', 'PostController@postView');
