9c50eaa0b0abb3a8ad2645c61692ae6466454f25
Ebenezer Omeonu <ebene2009@gmail.com>
2020-01-30 19:18:45 +0100

    freshh

--- routes/web.php
+++ routes/web.php
@@ -50,9 +50,46 @@
 // Route::get('/post/{id}/{name}/{password}', 'PostController@postView');
 use Illuminate\Support\Facades\DB;
 
+
+/*
+|--------------------------------------------------------------------------
+| Reading databases using raw sql
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
 Route::get('/insert', function(){
             DB::insert('insert into posts(title, content, author, is_admin) values(?, ?, ?, ?)', ['Php tutorial', 'Laravel is what i am learning', 'Ebene', 0]);
     }
 
 
 );
+// Route::get('/read', function(){
+
+//    $results= DB::select('select * from posts where id = ?', [1]);
+
+//    foreach($results as $result){
+
+//         return var_dump($result);
+//    }
+
+// });
+
+// Route::get('/update', function(){
+
+//     $results = DB::update('update posts set title = "New Title"  where id = ?', [1]);
+//     return $results;
+
+// });
+
+Route::get('/delete', function(){
+
+    $results = DB::delete('delete posts where id = ?', [1]);
+
+    return $results;
+
+});
